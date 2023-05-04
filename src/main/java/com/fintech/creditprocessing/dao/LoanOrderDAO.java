620f7412e537932c0208b54b8872ea6f1a592ca2
Vladiisss <vladislav-kur950@yandex.ru>
2023-05-04 17:39:35 +0300

    add job application

--- src/main/java/com/fintech/creditprocessing/dao/LoanOrderDAO.java
+++ src/main/java/com/fintech/creditprocessing/dao/LoanOrderDAO.java
@@ -2,7 +2,6 @@ package com.fintech.creditprocessing.dao;
 
 import com.fintech.creditprocessing.constant.Status;
 import com.fintech.creditprocessing.dao.mapper.StatusMapper;
-import com.fintech.creditprocessing.dto.LoanOrderDTO;
 import com.fintech.creditprocessing.entity.LoanOrder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -24,7 +24,9 @@ public class LoanOrderDAO {
             " credit_rating, status, time_insert, time_update) VALUES (?, ?, ?, ?, ?, ?, ?)";
     private static final String SQL_GET_ORDERS_BY_ID = "SELECT * FROM loan_order WHERE user_id=?";
     private static final String SQL_GET_STATUS_BY_ID = "SELECT status FROM loan_order WHERE order_id=?";
-    private static final String SQL_DELETE_ORDER_BY_ID  = "DELETE FROM loan_order WHERE order_id=?";
+    private static final String SQL_DELETE_ORDER_BY_ID = "DELETE FROM loan_order WHERE order_id=?";
+    private static final String SQL_GET_ORDERS_IN_PROGRESS = "SELECT * FROM loan_order WHERE status='IN_PROGRESS'";
+    private static final String SQL_UPDATE_ORDERS = "UPDATE loan_order SET status=?, time_update=? WHERE id=?";
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -50,6 +52,19 @@ public class LoanOrderDAO {
 
     public void delete(String orderId) {
         jdbcTemplate.update(SQL_DELETE_ORDER_BY_ID, orderId);
+    }
+
+    public List<LoanOrder> getOrdersInProgress() {
+        return jdbcTemplate.query(SQL_GET_ORDERS_IN_PROGRESS, new BeanPropertyRowMapper<>(LoanOrder.class));
+    }
+
+    //@Transactional
+    public void updateOrders(List<LoanOrder> loanOrders) {
+
+        List<Object[]> batchArgs = new ArrayList<>();
+        loanOrders.forEach(order -> batchArgs.add(new Object[]{order.getStatus().toString(), order.getTimeUpdate(), order.getId()}));
+
+        jdbcTemplate.batchUpdate(SQL_UPDATE_ORDERS, batchArgs);
     }
 
 }
